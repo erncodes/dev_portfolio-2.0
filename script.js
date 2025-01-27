8bef651c89bc674c7d19cdf6ef7852583dd82f87
Ernest Godla <erncodes@gmail.com>
2025-01-27 17:22:37 +0200

    about-section-update

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let menu = document.querySelector(".menu");
+let menu_icon = document.getElementById("menu");
 
 function ToggleMenu(){
     menu.classList.toggle("active");
+    if(menu_icon.className !== 'fa fa-xmark')
+    menu_icon.classList.replace('fa-bars','fa-xmark');
+    else
+    menu_icon.classList.replace('fa-xmark','fa-bars');
+
+    console.log(menu_icon.className)
 }
